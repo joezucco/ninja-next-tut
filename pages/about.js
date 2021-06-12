b1acd6e0d66e1d4467626ac598060b52d4b71501
Joseph Zucco <josephzucco@Josephs-MacBook-Pro.local>
2021-06-12 14:56:33 -0400

    lesson 8

---
+++ pages/about.js
@@ -0,0 +1,36 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut hendrerit
+        velit non nisl convallis, id sagittis enim cursus. Praesent ullamcorper
+        tristique turpis, a posuere ante ultricies ut. Phasellus vehicula odio
+        interdum nibh volutpat, id semper libero rhoncus. Mauris lorem eros,
+        ultrices ac felis a, congue pellentesque massa. Curabitur ullamcorper
+        vulputate purus, et aliquet elit volutpat ut. Curabitur at elementum
+        nisl. Praesent luctus tellus non eros egestas, ac finibus metus
+        hendrerit. Morbi dictum lacus a est accumsan, in hendrerit neque
+        convallis. Aliquam iaculis condimentum bibendum. Maecenas in lobortis
+        est, quis pellentesque dolor. Aenean justo metus, laoreet sed aliquet
+        sit amet, aliquet at nibh. Suspendisse ultricies vulputate lacus, quis
+        mollis nunc molestie nec. Maecenas ac nisl sodales, sagittis ligula id,
+        laoreet mauris.
+      </p>
+      <p>
+        Praesent gravida ex sed mauris sodales ultricies. Suspendisse vitae
+        neque non dolor fermentum convallis eu in libero. Cras ornare arcu
+        rutrum metus ultricies luctus. Nulla rhoncus augue nibh. Interdum et
+        malesuada fames ac ante ipsum primis in faucibus. Aenean at bibendum
+        metus. Maecenas euismod lectus sed feugiat lacinia. Sed in bibendum
+        enim. Curabitur quis ligula scelerisque, hendrerit justo a, tempus elit.
+        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+        inceptos himenaeos. Morbi ut tempus eros, eget sagittis eros. Mauris
+        semper scelerisque laoreet. Integer sit amet odio lectus. Etiam eu
+        pulvinar sapien.
+      </p>
+    </div>
+  );
+};
+
+export default About;
