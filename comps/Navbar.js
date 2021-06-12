e9ae5971f4644b44a7120107a941f1b53acd5007
Joseph Zucco <josephzucco@Josephs-MacBook-Pro.local>
2021-06-12 15:11:48 -0400

    add img

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,20 +1,21 @@
-import Link from "next/link";
+import Link from 'next/link';
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <nav>
-        <div className="logo">
-          <h1>Ninja List</h1>
-        </div>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-        <Link href="/ninjas">
-          <a>Ninja Listing</a>
-        </Link>
+      <div className="logo">
+        <img src="/logo.png" />
+      </div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/ninjas">
+        <a>Ninja Listing</a>
+      </Link>
     </nav>
   );
 };
